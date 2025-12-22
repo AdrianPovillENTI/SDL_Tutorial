19c81b15600eeeacf005623ea42c0448abe54db1
Xasa7980 <javier.sanchez@enti.cat>
2025-12-22 05:29:39 +0100

    Added level 1 boss

--- SDL3_Base/source/Enemies/Tentakor.cpp
+++ SDL3_Base/source/Enemies/Tentakor.cpp
@@ -1,1 +1,66 @@
 #include "Tentakor.h"
+
+Tentakor::Tentakor ()
+    : Enemy (
+    0 ,  // speed
+    100 ,  // health
+    5 ,  // damage
+    "resources/Player/Player.png"
+    )
+{
+
+    Start ( );
+}
+
+void Tentakor::Start ( )
+{
+    playOnStart = false;
+    LinearPattern * stop = new LinearPattern ( Vector2::Zero , speed );
+    bulletSpawnPoints = 
+    {
+        Vector2 ( 0.f , -45.f ),
+        Vector2 ( 0.f , -30.f ),
+        Vector2 ( 0.f , -15.f ),
+        Vector2 ( 0.f , 0.f ),
+        Vector2 ( 0.f , 15.f ),
+        Vector2 ( 0.f , 30.f ),
+        Vector2 ( 0.f , 45.f )
+    };
+    pattern = new EnemyBehaviourPattern (
+        new MultiPhasePattern 
+        ( {
+            { stop, 0 }
+        } )
+    );
+
+    Enemy::Start ( );
+}
+
+void Tentakor::Shoot ( )
+{ 
+    int randBulletAmount = randBulletAmount = rand ( ) % 5 + 3;
+    for ( int i = 0; i < randBulletAmount; i++ )
+    {
+        int randIndex = 0;
+        do
+        {
+            randIndex = rand ( ) % bulletSpawnPoints.size ( );
+        }
+        while ( lastPosIndex == randIndex );
+
+        EnemyBullet * b = new EnemyBullet ( bulletAnimationSprites , bulletSpeed , bulletDamage );
+        b->GetTransform ( )->position = _transform->position + bulletSpawnPoints [ randIndex ];
+        lastSpawnPos = bulletSpawnPoints [ randIndex ];
+        SPAWNER.SpawnObject ( b );
+    }
+}
+
+void Tentakor::Update ( )
+{
+    shootCooldown += 1.f / 60.f;
+    if ( shootCooldown >= timeToShoot )
+    {
+        Shoot();
+        shootCooldown = 0.f;
+    }
+}
---
+++ SDL3_Base/source/EnemyBullet.cpp
@@ -0,0 +1,35 @@
+#include "EnemyBullet.h"
+#include "Enemies/Enemy.h"
+
+#include "RenderManager.h"
+
+EnemyBullet::EnemyBullet ( const std::vector<std::string> & _anim , float _speed , int _damage )
+	: GameObject ( _anim [ 0 ] ) , Animator ( _anim ) ,
+	damage ( _damage ) , speed ( _speed )
+{
+	_transform->scale = Vector2 ( 2 , 2 );
+	_physics->AddCollider ( new AABB ( _transform->position , _transform->size ) );
+}
+void EnemyBullet::Start ( )
+{
+}
+
+void EnemyBullet::Update ( )
+{
+	GameObject::Update ( );
+
+	_physics->SetVelocity ( Vector2 ( -speed , 0.f ) );
+
+	_renderer->SetResourcePath ( ChangeSpriteFrame ( ) );
+
+	if ( _transform->position.x > RM->WINDOW_WIDTH * 1.4f || _transform->position.x < 0 ||
+		_transform->position.y > RM->WINDOW_HEIGHT * 1.3f || _transform->position.y < 0 )
+		Destroy ( );
+}
+
+void EnemyBullet::OnCollision ( Object * other )
+{
+	if ( dynamic_cast< Player * > ( other ) )
+		Destroy ( );
+}
+
